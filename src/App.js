008de7b9d54df1f31c1553c4980727f371eccb86
praja1905 <prajktap656@gmail.com>
2025-05-07 15:58:28 +0530

    Initial commit or updated anchor tags

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Shringar from './components/Shringar';
-import VastraKosh from './components/VastraKosh';
-import Resources from './components/Resources';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Shringar from "./components/Shringar";
+import VastraKosh from "./components/VastraKosh";
+import Resources from "./components/Resources";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Newnav from "./components/Newnav";
+
 
 function App() {
+  const location = useLocation();
+
+  // Show Newnav on all pages except the home ("/")
+  const showNewnav = location.pathname !== "/";
+
   return (
     <>
+      {showNewnav ? <Newnav /> : <Navbar />}
+
       <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/shringar" element={<Shringar />} />
