3e7c816207a8e0dac64b524f2061c7f5a8491d6e
eshapa <eshapatil2812@gmail.com>
2025-05-07 15:26:53 +0530

    Initial commit  full code

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './components/Navbar';
 // import Resources from './components/Resources';
 // import Contact from './components/Contact';
 import Footer from './components/Footer';
+import EmailPopup from './components/EmailPopup';
+ import Roll from './components/Roll';
 //import Etheniccards from './components/Etheniccards';
 import Fashion from './components/Fashion';
  import ExploreDesigners from './components/ExploreDesigner';
@@ -13,16 +15,20 @@ import ExploreTailor from './components/ExploreTailor';
 function App() {
   return (
     <>
-      <Routes>
+    <EmailPopup></EmailPopup>
+ 
+     
+           <Routes>
         <Route path="/" element={<Navbar />} />
         <Route path="/fashion" element={<Fashion/>} />
         <Route path="/ExploreDesigner" element={<ExploreDesigners />} />
         <Route path="/ExploreTailor" element={<ExploreTailor />} />
+        <Route path="/Roll" element={<Roll />} />
         {/* <Route path="/vastrakosh" element={<VastraKosh />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/contact" element={<Contact />} /> */}
       </Routes>
-
+  
       <Footer />
     </>
   );
