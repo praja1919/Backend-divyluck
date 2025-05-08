405454a9364e4c259f00b6961228e044a059143d
Prajkta Rajendra Patil <prajktapati1905@gmail.com>
2025-05-08 11:46:37 +0530

    Add files via upload
    src files

--- App.js
+++ App.js
@@ -1,34 +1,31 @@
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-// import S from './components/Etheniccards';
-// import VastraKosh from './components/VastraKosh';
-// import Resources from './components/Resources';
-// import Contact from './components/Contact';
-import Footer from './components/Footer';
-import EmailPopup from './components/EmailPopup';
- import Roll from './components/Roll';
-//import Etheniccards from './components/Etheniccards';
-import Fashion from './components/Fashion';
- import ExploreDesigners from './components/ExploreDesigner';
-import ExploreTailor from './components/ExploreTailor';
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Shringar from "./components/Shringar";
+import VastraKosh from "./components/VastraKosh";
+import Resources from "./components/Resources";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Newnav from "./components/Newnav";
+
 
 function App() {
+  const location = useLocation();
+
+  // Show Newnav on all pages except the home ("/")
+  const showNewnav = location.pathname !== "/";
+
   return (
     <>
-    <EmailPopup></EmailPopup>
- 
-     
-           <Routes>
+      {showNewnav ? <Newnav /> : <Navbar />}
+
+      <Routes>
         <Route path="/" element={<Navbar />} />
-        <Route path="/fashion" element={<Fashion/>} />
-        <Route path="/ExploreDesigner" element={<ExploreDesigners />} />
-        <Route path="/ExploreTailor" element={<ExploreTailor />} />
-        <Route path="/Roll" element={<Roll />} />
-        {/* <Route path="/vastrakosh" element={<VastraKosh />} />
+        <Route path="/shringar" element={<Shringar />} />
+        <Route path="/vastrakosh" element={<VastraKosh />} />
         <Route path="/resources" element={<Resources />} />
-        <Route path="/contact" element={<Contact />} /> */}
+        <Route path="/contact" element={<Contact />} />
       </Routes>
-  
+
       <Footer />
     </>
   );
